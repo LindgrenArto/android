d56b9e5c9e204137e51df36f3f3e424551ec1fd9
Flameri <arto.lindgren1@gmail.com>
2019-09-12 14:54:11 +0300

    Heads or tails progresses

--- app/src/main/java/com/example/helloworld/GuessActivity.java
+++ app/src/main/java/com/example/helloworld/GuessActivity.java
@@ -2,6 +2,8 @@ package com.example.helloworld;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,10 +11,32 @@ public class GuessActivity extends AppCompatActivity {
 
     private static final String TAG = "Guess activity";
 
+    private Button heads;
+    private Button tails;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
         Log.i(TAG, "Guess Activity launched!");
+
+        heads = findViewById(R.id.heads);
+        tails = findViewById(R.id.tails);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.heads:
+                Log.i(TAG, "heads");
+                break;
+
+            case R.id.tails:
+                Log.i(TAG, "tails");
+                break;
+
+                default:
+                    break;
+
+        }
     }
 }
